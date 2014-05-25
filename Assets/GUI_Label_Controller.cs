ed1430329c6f74552de5325cfc29d093c752f5e9
ChristianT <ChristianT@onlyentertainment.de>
2014-05-25 20:53:17 +0200

    badum

---
+++ Assets/GUI_Label_Controller.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI_Label_Controller : MonoBehaviour
+{
+
+    public Translation translation;
+
+    public UILabel characterName;
+    public UILabel race;
+    public UILabel raceName;
+    public UILabel gender;
+    public UILabel genderMale;
+    public UILabel genderFemale;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        RefreshLabels(null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RefreshLabels(UIPopupList popupList)
+    {
+        if (popupList != null) translation.SetLanguage(popupList.value);
+
+        characterName.text = translation.GetText("CharacterName");
+        race.text = translation.GetText("race");
+        raceName.text = translation.GetText("raceName");
+        gender.text = translation.GetText("gender");
+        genderMale.text = translation.GetText("GenderMale");
+        genderFemale.text = translation.GetText("GenderFemale");
+    }
+}
---
+++ Assets/Translation.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleXMLTranslation;
+using System;
+
+public class Translation : MonoBehaviour
+{
+    private SimpleTranslation translation;
+
+    public ENUMS.LANGUAGES language = ENUMS.LANGUAGES.english;
+
+    // Use this for initialization
+    void Awake()
+    {
+        string filePath = (Application.dataPath+"/").Replace("/","\\");
+        string fileName = "Translation.xml";
+
+        Debug.Log(filePath+fileName);
+        translation = new SimpleTranslation(filePath, fileName,"english");
+        //translation = new SimpleTranslation(filePath, fileName);
+        
+        Debug.Log(translation.GetLanguages()[0]);
+
+        translation.SetLanguage(language.ToString());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public string GetText(string keyword)
+    {
+        return translation.GetText(keyword);
+    }
+
+    public void SetLanguage(string language)
+    {
+        SetLanguage((ENUMS.LANGUAGES) Enum.Parse(typeof(ENUMS.LANGUAGES), language));
+    }
+
+    public void SetLanguage(ENUMS.LANGUAGES language)
+    {
+        this.language = language;
+        translation.SetLanguage(this.language.ToString());
+    }
+}
